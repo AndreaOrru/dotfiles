22b433756f1345b2f6428615683ad8e37644c6f2
Andrea Orru <andrea@orru.io>
2018-03-01 02:50:54 -0800

    MPD support for i3

---
+++ .config/i3/i3status.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+i3status --config ~/.config/i3/i3status.conf | while :
+do
+	read line
+    playing=$(mpc current)
+	if [ -z $playing ]; then
+        echo $line
+	else
+		status="▶"
+		mpc | grep -q "^\[paused\]" && status="■"
+		playing="$status $playing"
+        playing="[{ \"full_text\": \"${playing//\"/\\\"} \" },"
+        echo "${line/[/$playing}"
+	fi
+done
