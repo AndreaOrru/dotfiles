1f692d11e8c42ef9824d0c7983c2436596234606
Andrea Orru <andrea@orru.io>
2018-03-01 03:05:15 -0800

    Fix ssh-agent for good

--- .config/i3/i3status.sh
+++ .config/i3/i3status.sh
@@ -2,15 +2,15 @@
 
 i3status --config ~/.config/i3/i3status.conf | while :
 do
-	  read line
-    playing=$(mpc current)
-  	if [ -z $playing ]; then
-        echo $line
-  	else
-	    	status="▶"
-    		mpc | grep -q "^\[paused\]" && status="■"
-		    playing="$status $playing"
-        playing="[{ \"full_text\": \"${playing//\"/\\\"} \" },"
-        echo "${line/[/$playing}"
-  	fi
+  read line
+  playing=$(mpc current)
+  if [ -z $playing ]; then
+    echo $line
+  else
+    status="▶"
+    mpc | grep -q "^\[paused\]" && status="■"
+    playing="$status $playing"
+    playing="[{ \"full_text\": \"${playing//\"/\\\"} \" },"
+    echo "${line/[/$playing}"
+  fi
 done
