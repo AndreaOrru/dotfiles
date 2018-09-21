2ceb80dff36be3466378ffaa1f8e2a2fe0ac0b7f
Andrea Orru <andrea@orru.io>
2018-09-21 02:15:55 +0100

    Less layouts

--- user/.config/awesome/rc.lua
+++ user/.config/awesome/rc.lua
@@ -58,7 +58,6 @@ modkey = "Mod4"
 awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.tile.bottom,
-    awful.layout.suit.fair,
     awful.layout.suit.magnifier,
     awful.layout.suit.floating,
 }
