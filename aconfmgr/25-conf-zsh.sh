96b9212da07a0770ef2151c33a4237a9bb2a95d6
Andrea Orru <andrea@orru.io>
2018-09-21 01:16:02 +0100

    Start X at login on the NUC

--- aconfmgr/25-conf-zsh.sh
+++ aconfmgr/25-conf-zsh.sh
@@ -2,3 +2,12 @@ CopyFile /etc/profile.d/homebin.sh
 
 CopyFile /etc/zsh/zshenv
 CopyFile /etc/zsh/zshrc.local
+
+# Start X at login on the NUC.
+if [[ "$HOSTNAME" == "toxicity" ]]; then
+cat >> "$(GetPackageOriginalFile zsh /etc/zsh/zprofile)" <<'EOF'
+if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
+  exec startx
+fi
+EOF
+fi
