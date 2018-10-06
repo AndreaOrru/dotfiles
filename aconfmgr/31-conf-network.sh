f98bfd4c62d2b28051066483924b58c856b08e71
Andrea Orru <andrea@orru.io>
2018-10-06 07:19:25 +0100

    Move to conf-network
    Former-commit-id: 00f8cc157b72b8cfb65c81df03124b2c5e6115bd
    Former-commit-id: 1185a04155ff9b4516466fe16d94c1360ec04a29

--- aconfmgr/31-conf-network.sh
+++ aconfmgr/31-conf-network.sh
@@ -5,6 +5,17 @@ if [[ "$HOSTNAME" == "toxicity" ]]; then
 else
   # No custom DNS on laptop (because of WiFi hotspots).
   IgnorePath /etc/resolv.conf
+
+  # Wireless connections.
+  CopyFile /etc/NetworkManager/system-connections/AndyPhone 600
+  CopyFile /etc/NetworkManager/system-connections/BTBHub6-H5MP 600
+  CopyFile /etc/NetworkManager/system-connections/Dot11 600
+  CopyFile /etc/NetworkManager/system-connections/GAP-5G 600
+  CopyFile /etc/NetworkManager/system-connections/NETGEAR_5GEXT 600
+  CopyFile /etc/NetworkManager/system-connections/Printroom\ Cafe 600
+  CopyFile /etc/NetworkManager/system-connections/TNCAP617AA5\ 5G 600
+  CopyFile /etc/NetworkManager/system-connections/VM9882114 600
+  CopyFile /etc/NetworkManager/system-connections/Wireless-N 600
 fi
 
 # Firewall.
