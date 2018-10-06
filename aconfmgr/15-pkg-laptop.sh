e63c05b14a6c6cf1006e84e0fd7e0fa17e28d543
Andrea Orru <andrea@orru.io>
2018-10-06 07:18:14 +0100

    Wireless connections.
    Former-commit-id: 896dddeb0ebc0a7606445609c896f3df0fc6cbfa
    Former-commit-id: 41d8cfcf083556eee5d5ab20c11c3958f393d90e

--- aconfmgr/15-pkg-laptop.sh
+++ aconfmgr/15-pkg-laptop.sh
@@ -3,4 +3,15 @@ if [[ "$HOSTNAME" == "manhattan" ]]; then
   AddPackage networkmanager # Network connection manager and user applications
   AddPackage tlp # Linux Advanced Power Management
   AddPackage x86_energy_perf_policy # Read or write MSR_IA32_ENERGY_PERF_BIAS
+
+  # Wireless connections.
+  CopyFile /etc/NetworkManager/system-connections/AndyPhone 600
+  CopyFile /etc/NetworkManager/system-connections/BTBHub6-H5MP 600
+  CopyFile /etc/NetworkManager/system-connections/Dot11 600
+  CopyFile /etc/NetworkManager/system-connections/GAP-5G 600
+  CopyFile /etc/NetworkManager/system-connections/NETGEAR_5GEXT 600
+  CopyFile /etc/NetworkManager/system-connections/Printroom\ Cafe 600
+  CopyFile /etc/NetworkManager/system-connections/TNCAP617AA5\ 5G 600
+  CopyFile /etc/NetworkManager/system-connections/VM9882114 600
+  CopyFile /etc/NetworkManager/system-connections/Wireless-N 600
 fi
