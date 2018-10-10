5ea7097857402a9643b923b6de17c999307f9b16
Andrea Orru <andrea@orru.io>
2018-10-10 14:14:23 +0200

    +1 wireless
    Former-commit-id: 9209de634e8b1211dae755292bc34d27e0fbf5f3
    Former-commit-id: 379f5f81b7d6d5e2ad9a3ae3e668c714c70c6fc9

--- aconfmgr/31-conf-network.sh
+++ aconfmgr/31-conf-network.sh
@@ -14,6 +14,7 @@ else
   CopyFile /etc/NetworkManager/system-connections/NETGEAR_5GEXT 600
   CopyFile /etc/NetworkManager/system-connections/Printroom\ Cafe 600
   CopyFile /etc/NetworkManager/system-connections/TNCAP617AA5\ 5G 600
+  CopyFile /etc/NetworkManager/system-connections/TP-LINK_F35C 600
   CopyFile /etc/NetworkManager/system-connections/VM9882114 600
   CopyFile /etc/NetworkManager/system-connections/WebPocket-53E0 600
   CopyFile /etc/NetworkManager/system-connections/Wireless-N 600
