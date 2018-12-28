3cb0f2cc40096e182d40834945c89c7212c93c94
Andrea Orru <andrea@orru.io>
2018-12-28 17:11:15 +0100

    Mpdris2 packages
    Former-commit-id: 5e46a694953450e1c81fefd02296cb41bc2607e1

--- aconfmgr/.config/aconfmgr/70-desktop.sh
+++ aconfmgr/.config/aconfmgr/70-desktop.sh
@@ -44,6 +44,10 @@ if [ "$LAPTOP" == true ]; then
   AddPackage dconf-editor # dconf Editor
   AddPackage gnome-tweaks # Graphical interface for advanced GNOME 3 settings (Tweak Tool)
 
+  # MPD integration with Gnome.
+  AddPackage --foreign gnome-shell-extension-mediaplayer-git # A mediaplayer indicator for the Gnome Shell
+  AddPackage --foreign mpdris2-py3-git # MPRIS2 support for MPD using Python 3 -- git version
+
   # Gnome/GTK theme.
   AddPackage --foreign vimix-gtk-themes-git # Vimix is a flat Material Design theme for GTK 3, GTK 2 and Gnome-Shell
   AddPackage arc-icon-theme # Arc icon theme. Official releases only.
