42d4e20fbdef664b507d83061e757f6c6e4cdab7
Andrea Orru <andrea@orru.io>
2019-03-04 15:06:15 +0900

    Remove postman
    Former-commit-id: 80984bbef12cbe8644eb32b1254ed75994faeefc

--- aconfmgr/.config/aconfmgr/80-apps.sh
+++ aconfmgr/.config/aconfmgr/80-apps.sh
@@ -5,7 +5,6 @@ AddPackage mpv # a free, open source, and cross-platform media player
 AddPackage pepper-flash # Adobe Flash Player PPAPI
 AddPackage pgadmin4 # Comprehensive design and management interface for PostgreSQL
 AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
-AddPackage --foreign postman-bin # Build, test, and document your APIs faster
 AddPackage --foreign profile-sync-daemon # Syncs browser profiles to tmpfs reducing SSD/HDD calls and speeding-up browsers.
 AddPackage --foreign signal-desktop-bin # Private messaging from your desktop
 AddPackage --foreign spotify # A proprietary music streaming service
