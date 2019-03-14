cb1cd81e8ecff13895808839879046ead099eed5
Andrea Orru <andrea@orru.io>
2019-03-14 15:06:34 +0000

    Delete profile-sync-daemon
    Former-commit-id: ac17728212924052be2e8907de03a50453b9e927

--- aconfmgr/.config/aconfmgr/80-apps.sh
+++ aconfmgr/.config/aconfmgr/80-apps.sh
@@ -4,7 +4,6 @@ AddPackage kitty # A modern, hackable, featureful, OpenGL based terminal emulato
 AddPackage mpv # a free, open source, and cross-platform media player
 AddPackage pgadmin4 # Comprehensive design and management interface for PostgreSQL
 AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
-AddPackage --foreign profile-sync-daemon # Syncs browser profiles to tmpfs reducing SSD/HDD calls and speeding-up browsers.
 AddPackage --foreign signal-desktop-bin # Private messaging from your desktop
 AddPackage --foreign spotify # A proprietary music streaming service
 AddPackage youtube-viewer # Command line utility for viewing YouTube videos
