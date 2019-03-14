0c790e753c6b7dad1679099799e41b7c170a40c0
Andrea Orru <andrea@orru.io>
2019-03-14 16:55:21 +0000

    Some extra Firefox stuff
    Former-commit-id: 1a5cdadfb4af92f632424dd3ef47513ee47900da

--- aconfmgr/.config/aconfmgr/80-apps.sh
+++ aconfmgr/.config/aconfmgr/80-apps.sh
@@ -1,15 +1,19 @@
+# Firefox.
 AddPackage firefox-developer-edition # Developer Edition of the popular Firefox web browser
+AddPackage hunspell-en_GB # GB English hunspell dictionaries
+CopyFile /etc/profile.d/mozilla-xinput2.sh # Enable touchscreen on Firefox.
+
 AddPackage gimp # GNU Image Manipulation Program
 AddPackage kitty # A modern, hackable, featureful, OpenGL based terminal emulator
 AddPackage mpv # a free, open source, and cross-platform media player
 AddPackage pgadmin4 # Comprehensive design and management interface for PostgreSQL
 AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
 AddPackage --foreign signal-desktop-bin # Private messaging from your desktop
-AddPackage --foreign spotify # A proprietary music streaming service
 AddPackage youtube-viewer # Command line utility for viewing YouTube videos
 
-# Hi-DPI aware Spotify wrapper.
-CopyFile /usr/local/bin/spotify 755
+# Spotify.
+AddPackage --foreign spotify # A proprietary music streaming service
+CopyFile /usr/local/bin/spotify 755 # Hi-DPI aware Spotify wrapper.
 
 
 if [ "$WORKSTATION" == true ]; then
