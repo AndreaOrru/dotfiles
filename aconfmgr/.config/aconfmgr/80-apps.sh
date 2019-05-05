926df22138a6628aae6d6ec66b00779319d92515
Andrea Orru <andrea@orru.io>
2019-05-05 09:55:49 +0100

    Remove Telegram
    Former-commit-id: 7cb7eed15efb3618c0e318610196df0bb2890716

--- aconfmgr/.config/aconfmgr/80-apps.sh
+++ aconfmgr/.config/aconfmgr/80-apps.sh
@@ -17,5 +17,4 @@ AddPackage pgadmin4 # Comprehensive design and management interface for PostgreS
 AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
 AddPackage --foreign signal-desktop-bin # Private messaging from your desktop
 AddPackage sqlitebrowser # SQLite Database browser is a light GUI editor for SQLite databases, built on top of Qt
-AddPackage telegram-desktop # Official Telegram Desktop client
 AddPackage youtube-viewer # Command line utility for viewing YouTube videos
