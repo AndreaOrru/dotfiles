a935061ae27a7d1170da1a87730215824bdb9729
Andrea Orru <andrea@orru.io>
2019-06-08 20:28:23 +0100

    Wine, bsnes, kitty, cleanup
    Former-commit-id: 062326d2f8aadb9efd0cc5cdd93d36cb414af5d4

--- aconfmgr/.config/aconfmgr/80-apps.sh
+++ aconfmgr/.config/aconfmgr/80-apps.sh
@@ -8,13 +8,16 @@ CopyFile /etc/profile.d/mozilla-xinput2.sh # Enable touchscreen on Firefox.
 AddPackage --foreign spotify # A proprietary music streaming service
 CopyFile /usr/local/bin/spotify 755 # Hi-DPI aware Spotify wrapper.
 
+# Wine.
+AddPackage wine-staging # A compatibility layer for running Windows programs - Staging branch
+AddPackage wine_gecko # Wine's built-in replacement for Microsoft's Internet Explorer
+AddPackage winetricks # Script to install various redistributable runtime libraries in Wine.
+
 # Others.
 AddPackage anki # Helps you remember facts (like words/phrases in a foreign language) efficiently
+AddPackage --foreign bsnes-plus-git # debug-oriented fork of bsnes v073
 AddPackage gimp # GNU Image Manipulation Program
-AddPackage kitty # A modern, hackable, featureful, OpenGL based terminal emulator
+AddPackage --foreign kitty-git # A modern, hackable, featureful, OpenGL based terminal emulator
 AddPackage mpv # a free, open source, and cross-platform media player
-AddPackage pgadmin4 # Comprehensive design and management interface for PostgreSQL
 AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
-AddPackage --foreign signal-desktop-bin # Private messaging from your desktop
-AddPackage sqlitebrowser # SQLite Database browser is a light GUI editor for SQLite databases, built on top of Qt
 AddPackage youtube-viewer # Command line utility for viewing YouTube videos
