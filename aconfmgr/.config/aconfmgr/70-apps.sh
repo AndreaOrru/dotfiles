626f616a125e8b8975a2cb00524f7b318127c2ca
Andrea Orru <andrea@orru.io>
2019-08-02 15:03:25 +0100

    Add profile cleaner
    Former-commit-id: 0e15a39d4be4f05245d34339761d749325e63e1f

--- aconfmgr/.config/aconfmgr/70-apps.sh
+++ aconfmgr/.config/aconfmgr/70-apps.sh
@@ -2,6 +2,7 @@ AddPackage gimp # GNU Image Manipulation Program
 AddPackage --foreign google-chrome # The popular and trusted web browser by Google (Stable Channel)
 AddPackage mpv # a free, open source, and cross-platform media player
 AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
+AddPackage --foreign profile-cleaner # Reduces profile size by cleaning their sqlite databases.
 AddPackage --foreign spotify # A proprietary music streaming service
 AddPackage termite # A simple VTE-based terminal
 AddPackage youtube-viewer # Command line utility for viewing YouTube videos
