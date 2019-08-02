71dc8af770d769ffee702ac91dcc63cb5b8d0f0a
Andrea Orru <andrea@orru.io>
2019-08-02 21:03:24 +0100

    Firefox is better
    Former-commit-id: a7d534be88c2970f0e0a2cc8cf6172d0cdc2fa0f

--- aconfmgr/.config/aconfmgr/70-apps.sh
+++ aconfmgr/.config/aconfmgr/70-apps.sh
@@ -1,9 +1,10 @@
 AddPackage firefox # Standalone web browser from mozilla.org
+AddPackage hunspell-en_US # US English hunspell dictionaries
+AddPackage --foreign profile-cleaner # Reduces profile size by cleaning their sqlite databases.
+
 AddPackage gimp # GNU Image Manipulation Program
-AddPackage --foreign google-chrome-dev # The popular and trusted web browser by Google (Dev Channel)
 AddPackage mpv # a free, open source, and cross-platform media player
 AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
-AddPackage --foreign profile-cleaner # Reduces profile size by cleaning their sqlite databases.
 AddPackage --foreign spotify # A proprietary music streaming service
 AddPackage termite # A simple VTE-based terminal
 AddPackage youtube-viewer # Command line utility for viewing YouTube videos
