dcfe4fae8039244a3e97e7329540d931e6bda14e
Andrea Orru <andrea@orru.io>
2019-08-02 21:04:43 +0100

    ...but I still need Chromium for some things
    Former-commit-id: 34582a3e69423d9db03d50166b8696424efec8cc

--- aconfmgr/.config/aconfmgr/70-apps.sh
+++ aconfmgr/.config/aconfmgr/70-apps.sh
@@ -2,6 +2,7 @@ AddPackage firefox # Standalone web browser from mozilla.org
 AddPackage hunspell-en_US # US English hunspell dictionaries
 AddPackage --foreign profile-cleaner # Reduces profile size by cleaning their sqlite databases.
 
+AddPackage chromium # A web browser built for speed, simplicity, and security
 AddPackage gimp # GNU Image Manipulation Program
 AddPackage mpv # a free, open source, and cross-platform media player
 AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
