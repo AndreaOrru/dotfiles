afa505e2fb6e01c5f6f583ceb5ca7bc15bd40b71
Andrea Orru <andrea@orru.io>
2020-04-11 12:28:54 +0900

    Back to standard Firefox
    Former-commit-id: 6b88ce4e1452e700e1b1f7771a42dce64540ccd3

--- aconfmgr/.config/aconfmgr/70-apps.sh
+++ aconfmgr/.config/aconfmgr/70-apps.sh
@@ -1,6 +1,6 @@
 # Firefox.
 CopyFile /etc/profile.d/firefox-vars.sh
-AddPackage --foreign fedora-firefox-wayland-bin # Fedora's firefox build with wayland enabled by default
+AddPackage firefox # Standalone web browser from mozilla.org
 AddPackage hunspell-en_US # US English hunspell dictionaries
 AddPackage profile-cleaner # Simple script to vacuum and reindex sqlite databases used by browsers
 
