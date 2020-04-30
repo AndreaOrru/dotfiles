6b739627639bd597e1613cfa513dad11da73ce0e
Andrea Orru <andrea@orru.io>
2020-04-30 23:51:29 +0900

    A few more help functions
    Former-commit-id: 6526a24c78f22f9eeaf104460136aea11d3728a3

--- emacs/.emacs.d/lisp/init-help.el
+++ emacs/.emacs.d/lisp/init-help.el
@@ -6,6 +6,7 @@
 
 ;; Key bindings.
 (with-eval-after-load 'evil-leader
+  (evil-leader/set-key "ha" 'apropos)
   (evil-leader/set-key "hf" 'counsel-describe-function)
   (evil-leader/set-key "hk" 'describe-key)
   (evil-leader/set-key "hv" 'counsel-describe-variable))
