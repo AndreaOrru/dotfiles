450cf1b6ed93b0c1769dd6cb053dae2fa760cb57
Andrea Orru <andrea@orru.io>
2020-04-30 20:26:58 +0900

    Eshell prompt and aliases
    Former-commit-id: 0ba05abc043aa6bd9cce76c2b9cd1b2ea8f8cca2

--- emacs/.emacs.d/lisp/init-shell.el
+++ emacs/.emacs.d/lisp/init-shell.el
@@ -2,18 +2,29 @@
 (add-hook 'eshell-mode-hook
 	  (lambda () (setq-local global-hl-line-mode nil)))
 
+;; Eshell prompt theme and extra info.
+(require-package 'eshell-prompt-extras)
+(with-eval-after-load 'esh-opt
+  (autoload 'epe-theme-lambda "eshell-prompt-extras")
+  (setq eshell-highlight-prompt nil
+	eshell-prompt-function 'epe-theme-lambda))
+;; Eshell aliases.
+(defun eshell/d () (dired-other-window "."))
+(defalias 'eshell/e 'find-file-other-window)
+
 ;; Pop an Eshell at the bottom of the frame.
 (require-package 'shell-pop)
 (setq
  shell-pop-full-span t
  shell-pop-shell-type '("eshell" "*eshell" (lambda nil (eshell))))
 
-(global-set-key (kbd "s-x") 'shell-pop)
-(global-set-key (kbd "M-x") 'shell-pop)
-
 ;; TMux integration.
 (require-package 'tmux-pane)
 (require 'tmux-pane)
+
+;; Key bindings.
+(global-set-key (kbd "s-x") 'shell-pop)
+(global-set-key (kbd "M-x") 'shell-pop)
 (with-eval-after-load 'evil-leader
   (evil-leader/set-key "RET" 'tmux-pane-toggle-horizontal))
 
