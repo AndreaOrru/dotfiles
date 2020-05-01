a4da5c8b800256a2b7e5051bc89d2062f3ff8c8d
Andrea Orru <andrea@orru.io>
2020-05-02 01:04:11 +0900

    No littering
    Former-commit-id: db727db21fba50933dd89887e636b9393b2f1305

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,12 +1,11 @@
 ;; Add the lisp/ subdirectory to the load path.
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-;; Save customized variables in a separate file.
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
 
 ;; Load submodules.
 (require 'init-performance)
 (require 'init-elpa)
+(require 'init-no-littering)
+
 (require 'init-interface)
 (require 'init-modeline)
 
@@ -26,7 +25,6 @@
 (require 'init-lsp)
 (require 'init-misc)
 
-
-;; Load customized variables.
-(when (file-exists-p custom-file)
-  (load custom-file))
+;; Save customized variables in a separate file.
+(setq custom-file (no-littering-expand-etc-file-name "custom.el"))
+(when (file-exists-p custom-file) (load custom-file))
---
+++ emacs/.emacs.d/lisp/init-no-littering.el
@@ -0,0 +1,4 @@
+(require-package 'no-littering)
+(require 'no-littering)
+
+(provide 'init-no-littering)
