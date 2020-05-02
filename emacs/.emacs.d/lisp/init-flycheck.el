6c22726af9cefdf49edd6405ff411d82be973b29
Andrea Orru <andrea@orru.io>
2020-05-02 14:30:40 +0900

    No bell on Kitty, Flycheck fixes
    Former-commit-id: da065e0136da28261711b7bf89b1f2f30132ce76

--- emacs/.emacs.d/lisp/init-flycheck.el
+++ emacs/.emacs.d/lisp/init-flycheck.el
@@ -1,7 +1,10 @@
 (require-package 'flycheck)
 (add-hook 'after-init-hook 'global-flycheck-mode)
 
-;; Only run syntax check on open and save.
-(defvar flycheck-check-syntax-automatically '(save mode-enabled))
+(with-eval-after-load 'flycheck
+  ;; Disable Emacs Lisp syntax checking.
+  (setq flycheck-global-modes '(not emacs-lisp-mode))
+  ;; Only run syntax check on open and save.
+  (setq flycheck-check-syntax-automatically '(save mode-enabled)))
 
 (provide 'init-flycheck)
