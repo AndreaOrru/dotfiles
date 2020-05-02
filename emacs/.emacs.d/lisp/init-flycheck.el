ab726680334518a89c6163592e3ce8c5b336686f
Andrea Orru <andrea@orru.io>
2020-05-02 15:08:05 +0900

    Popup flycheck, SPC e
    Former-commit-id: e9d4afd0730c2950d5bf9165f4b85fe4b2510066

--- emacs/.emacs.d/lisp/init-flycheck.el
+++ emacs/.emacs.d/lisp/init-flycheck.el
@@ -2,9 +2,16 @@
 (add-hook 'after-init-hook 'global-flycheck-mode)
 
 (with-eval-after-load 'flycheck
+  ;; Use SPC e as prefix for Flycheck commands.
+  (evil-leader/set-key "e" flycheck-command-map)
   ;; Disable Emacs Lisp syntax checking.
   (setq flycheck-global-modes '(not emacs-lisp-mode))
   ;; Only run syntax check on open and save.
   (setq flycheck-check-syntax-automatically '(save mode-enabled)))
+
+;; Display Flycheck error list as a sticky popup.
+(with-eval-after-load 'popwin
+  (push '(flycheck-error-list-mode :stick t)
+        popwin:special-display-config))
 
 (provide 'init-flycheck)
