e448e878f975a972f68a349619e50f7231c34671
Andrea Orru <andrea@orru.io>
2020-05-04 03:04:30 +0900

    LSP/which-key integration
    Former-commit-id: f253612988f7a639772028888374eca93c5333cc

--- emacs/.emacs.d/config/init-lsp.el
+++ emacs/.emacs.d/config/init-lsp.el
@@ -3,10 +3,16 @@
 (require-package 'lsp-ui)
 (add-hook 'prog-mode-hook 'lsp-deferred)
 
-(add-hook 'lsp-mode-hook
-	  (lambda ()
-	    (evil-leader/set-key "l" lsp-command-map)  ;; SPC l as prefix for LSP commands.
-	    (setq lsp-prefer-capf t)                   ;; Use CAPF completion (more efficient).
-	    (setq lsp-keep-workspace-alive nil)))      ;; Kill language server when done.
+(after 'lsp-mode
+  ;; Enable which-key prefix descriptions.
+  (setq lsp-keymap-prefix "SPC l")
+  (add-hook 'lsp-mode-hook 'lsp-enable-which-key-integration)
+
+  (add-hook 'lsp-mode-hook
+	        (lambda () 
+	          (evil-leader/set-key "l" lsp-command-map)  ;; SPC l as prefix for LSP commands.
+              (setq lsp-auto-guess-root t)               ;; Guess the project root.
+	          (setq lsp-prefer-capf t)                   ;; Use CAPF completion (more efficient).
+	          (setq lsp-keep-workspace-alive nil))))     ;; Kill language server when done.
 
 (provide 'init-lsp)
