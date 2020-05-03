788bfd8dfbe02e46de3362d70f6efae2d6455228
Andrea Orru <andrea@orru.io>
2020-05-04 03:57:33 +0900

    Fix LSP options
    Former-commit-id: 06026ed67f7c68b99243f0a43c9e1ee39c2f96f7

--- emacs/.emacs.d/config/init-lsp.el
+++ emacs/.emacs.d/config/init-lsp.el
@@ -8,11 +8,13 @@
   (setq lsp-keymap-prefix "SPC l")
   (add-hook 'lsp-mode-hook 'lsp-enable-which-key-integration)
 
+  (setq lsp-keep-workspace-alive nil)  ;; Kill language server when done.
+  (setq lsp-prefer-capf t)             ;; Use CAPF completion (more efficient).
+  (setq lsp-auto-guess-root t)         ;; Guess the project root.
+
+  ;; SPC l as prefix for LSP commands.
   (add-hook 'lsp-mode-hook
             (lambda ()
-              (evil-leader/set-key "l" lsp-command-map)  ;; SPC l as prefix for LSP commands.
-              (setq lsp-keep-workspace-alive nil)        ;; Kill language server when done.
-              (setq lsp-prefer-capf t)                   ;; Use CAPF completion (more efficient).
-              (setq lsp-auto-guess-root t))))            ;; Guess the project root.
+              (evil-leader/set-key "l" lsp-command-map))))
 
 (provide 'init-lsp)
