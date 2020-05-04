584eaebd17e2fde30dbf796c613c2fea0e822eae
Andrea Orru <andrea@orru.io>
2020-05-04 15:34:40 +0900

    Fix Python configuration
    Former-commit-id: d0d45e1dc714f7d46702d15e82314b9f62de480c

--- emacs/.emacs.d/config/init-python.el
+++ emacs/.emacs.d/config/init-python.el
@@ -1,10 +1,20 @@
+;; Automatically enable the project's virtualenv.
 (require-package 'auto-virtualenvwrapper)
-(add-hook 'python-mode-hook
-          (lambda ()
-            ;; Automatically enable the project's virtualenv.
-            (auto-virtualenvwrapper-activate)
-            ;; Disable YAPF and autopep8 to prioritize Black.
-            (setq lsp-pyls-plugins-autopep8-enabled nil)
-            (setq lsp-pyls-plugins-yapf-enabled nil)))
+(add-hook 'python-mode-hook 'auto-virtualenvwrapper-activate)
+
+(after 'lsp-mode
+  ;; Enable Flake8.
+  (setq lsp-pyls-configuration-sources ["flake8"])
+  (setq lsp-pyls-plugins-flake8-enabled t)
+  ;; Disable plugins included inside Flake8.
+  (setq lsp-pyls-plugins-mccabe-enabled nil)
+  (setq lsp-pyls-plugins-pycodestyle-enabled nil)
+  (setq lsp-pyls-plugins-pyflakes-enabled nil)
+  ;; Disable PyLint.
+  (setq lsp-pyls-plugins-pylint-enabled nil)
+
+  ;; Disable YAPF and autopep8 to prioritize Black.
+  (setq lsp-pyls-plugins-autopep8-enabled nil)
+  (setq lsp-pyls-plugins-yapf-enabled nil))
 
 (provide 'init-python)
