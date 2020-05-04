a8a13db7b551b7a00648e0165ed9c1822eb46a94
Andrea Orru <andrea@orru.io>
2020-05-04 19:17:34 +0900

    \
    Former-commit-id: 7e5b6eb3c4ce8145876ed8b9203c55cc3452732e

--- emacs/.emacs.d/config/init-lsp.el
+++ emacs/.emacs.d/config/init-lsp.el
@@ -3,21 +3,18 @@
 (require-package 'lsp-ui)
 (add-hook 'prog-mode-hook 'lsp-deferred)
 
-(after 'lsp-mode
-  ;; Enable which-key prefix descriptions.
-  (setq lsp-keymap-prefix "SPC l")
+(after [lsp-mode evil]
+  ;; Set \ as the LSP prefix.
+  (setq lsp-keymap-prefix "\\")
+  (evil-define-key '(normal visual) lsp-mode-map "\\" lsp-command-map)
   (add-hook 'lsp-mode-hook 'lsp-enable-which-key-integration)
 
   (setq lsp-auto-guess-root t)            ;; Guess the project root.
   (setq lsp-flycheck-live-reporting nil)  ;; Flycheck live reporting.
   (setq lsp-keep-workspace-alive nil)     ;; Kill language server when done.
   (setq lsp-prefer-capf t)                ;; Use CAPF completion (more efficient).
-  ;; Format files before saving.
-  (add-hook 'before-save-hook 'lsp-format-buffer)
 
-  ;; SPC l as prefix for LSP commands.
-  (add-hook 'lsp-mode-hook
-            (lambda ()
-              (evil-leader/set-key "l" lsp-command-map))))
+  ;; Format files before saving.
+  (add-hook 'before-save-hook 'lsp-format-buffer))
 
 (provide 'init-lsp)
