8bbbf97a5c4765e896dcdeaa679b8504eaf1297f
Andrea Orru <andrea@orru.io>
2020-05-04 21:32:11 +0900

    Stick popup windows
    Former-commit-id: 53d5eebcc028d49d3179ecdba0ea777252d8e5de

--- emacs/.emacs.d/config/init-windows.el
+++ emacs/.emacs.d/config/init-windows.el
@@ -2,7 +2,12 @@
 (require-package 'popwin)
 (add-hook 'after-init-hook 'popwin-mode)
 (after 'popwin
-  (push 'apropos-mode popwin:special-display-config))
+  (add-to-list 'popwin:special-display-config 'apropos-mode))
+
+;; Optionally stick popup windows.
+(after 'init-evil
+  (which-key/describe-prefix "w" "windows")
+  (evil-leader/set-key "ws" 'popwin:stick-popup-window))
 
 ;; Split and switch to the new window immediately.
 (global-set-key (kbd "C-x 2") (lambda () (interactive) (split-window-below) (other-window 1)))
