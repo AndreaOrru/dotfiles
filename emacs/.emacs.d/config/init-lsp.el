71fba18592bfae7bbb9f28dec2bcce1e810709c6
Andrea Orru <andrea@orru.io>
2020-05-04 20:48:42 +0900

    Finally fix the LSP prefix
    Former-commit-id: 663e3c7f971edac6dbc956fba88be09647167469

--- emacs/.emacs.d/config/init-lsp.el
+++ emacs/.emacs.d/config/init-lsp.el
@@ -3,18 +3,22 @@
 (require-package 'lsp-ui)
 (add-hook 'prog-mode-hook 'lsp-deferred)
 
-(after [lsp-mode evil]
-  ;; Set \ as the LSP prefix.
+(after [lsp-mode init-evil]
+  ;; Enable which-key integration for LSP prefix.
   (setq lsp-keymap-prefix "\\")
-  (evil-define-key '(normal visual) lsp-mode-map (kbd "\\") lsp-command-map)
   (add-hook 'lsp-mode-hook 'lsp-enable-which-key-integration)
 
   (setq lsp-auto-guess-root t)            ;; Guess the project root.
   (setq lsp-flycheck-live-reporting nil)  ;; Flycheck live reporting.
   (setq lsp-keep-workspace-alive nil)     ;; Kill language server when done.
   (setq lsp-prefer-capf t)                ;; Use CAPF completion (more efficient).
-
   ;; Format files before saving.
-  (add-hook 'before-save-hook 'lsp-format-buffer))
+  (add-hook 'before-save-hook 'lsp-format-buffer)
+
+  ;; Enable the LSP prefix for LSP-enabled buffers.
+  (add-hook 'lsp-mode-hook
+            (lambda ()
+              (evil-local-set-key 'normal (kbd "\\") lsp-command-map)
+              (evil-local-set-key 'visual (kbd "\\") lsp-command-map))))
 
 (provide 'init-lsp)
