82f9babafc9081363226e9c0f00ad4822c8e7f7e
Andrea Orru <andrea@orru.io>
2020-05-08 20:41:54 +0900

    Remove ag
    Former-commit-id: 8bcdef7cf5e8bc7b8e3c379ff497f710a5bfa6e7

--- aconfmgr/.config/aconfmgr/40-shell.sh
+++ aconfmgr/.config/aconfmgr/40-shell.sh
@@ -21,7 +21,6 @@ AddPackage powerline # Statusline plugin for vim, and provides statuslines and p
 AddPackage ranger # Simple, vim-like file manager
 AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
 AddPackage stow # Manage installation of multiple softwares in the same directory tree
-AddPackage the_silver_searcher # Code searching tool similar to Ack, but faster
 AddPackage tmux # A terminal multiplexer
 AddPackage tree # A directory listing program displaying a depth indented list of files
 
