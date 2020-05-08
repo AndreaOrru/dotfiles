3fa0cf41b5d9eed24cfd950cd9a94eb86f21f4f9
Andrea Orru <andrea@orru.io>
2020-05-08 20:42:56 +0900

    Use dumb jump as a fallback to xref
    Former-commit-id: dd67d2eec7172316ba994344c7edc0fc59d6b633

--- emacs/.emacs.d/config/init-projects.el
+++ emacs/.emacs.d/config/init-projects.el
@@ -1,11 +1,21 @@
 ;; Enable Projectile.
 (require-package 'projectile)
 (add-hook 'after-init-hook 'projectile-mode)
-
 (after 'projectile
   (setq
    projectile-completion-system 'ivy  ;; Use Ivy for completion.
    projectile-enable-caching t))      ;; Faster file indexing.
+
+;; Enable dumb-jump as a fallback to search for definitions inside projects.
+(require-package 'dumb-jump)
+(defun my/xref-find-definitions (orig-fun &rest args)
+  (condition-case nil (apply orig-fun args)
+    (error (dumb-jump-go))))
+(defun my/xref-find-definitions-other-window (orig-fun &rest args)
+  (condition-case nil (apply orig-fun args)
+    (error (dumb-jump-go-other-window))))
+(advice-add 'xref-find-definitions :around 'my/xref-find-definitions)
+(advice-add 'xref-find-definitions-other-window :around 'my/xref-find-definitions-other-window)
 
 (defun edit-dotemacs ()
   "Open the directory of the current Emacs configuration."
