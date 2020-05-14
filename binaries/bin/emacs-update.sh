3521940d950a0458d1be4cb9993c4089b5fea27f
Andrea Orru <andrea@orru.io>
2020-05-14 22:50:55 +0900

    Update Emacs script
    Former-commit-id: 13350d4d0c43605251529109636d1f278c20d2f7

---
+++ binaries/bin/emacs-update.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+mkdir -p ~/dev/makepkg
+[ ! -d "$HOME/dev/makepkg/emacs27-git" ] && yay -G emacs27-git
+
+cd ~/dev/makepkg/emacs27-git
+rm -f emacs27-git-*
+rm -rf pkg src
+
+git reset --hard
+git pull
+sed -i 's/^LUCID=     /LUCID="YES"/' PKGBUILD
+
+makepkg -si --noconfirm
+rm -rf pkg src
+
+rm -rf ~/.emacs.d/elpa
+rm -rf ~/.emacs.d/etc
+rm -rf ~/.emacs.d/var
+
+systemctl --user daemon-reload
