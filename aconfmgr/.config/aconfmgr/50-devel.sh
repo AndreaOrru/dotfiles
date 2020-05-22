9c81373d0cb1998d1f59dad74a58dcd3300d0670
Andrea Orru <andrea@orru.io>
2020-05-22 19:31:21 +0900

    Rust, Pypy
    Former-commit-id: 09a4fb2b23d19fdb5eedbc9aaa8c72c3e34994d5

--- aconfmgr/.config/aconfmgr/50-devel.sh
+++ aconfmgr/.config/aconfmgr/50-devel.sh
@@ -10,12 +10,17 @@ AddPackage gdb # The GNU Debugger
 AddPackage git # the fast distributed version control system
 AddPackage git-crypt # Transparent file encryption in Git
 AddPackage --foreign git-extras # GIT utilities -- repo summary, commit counting, repl, changelog population and more
-AddPackage go # Core compiler tools for the Go programming language
 AddPackage hub # cli interface for Github
 AddPackage redis # Advanced key-value store
 AddPackage scons # Extensible Python-based build utility
+
+# Go, Rust.
+AddPackage go # Core compiler tools for the Go programming language
+AddPackage rust-analyzer # Experimental Rust compiler front-end for IDEs
+AddPackage rustup # The Rust toolchain installer
 
 # Python.
 AddPackage ipython # An enhanced Interactive Python shell.
 AddPackage --foreign python-pdbpp # pdb++, a drop-in replacement for pdb
 AddPackage python-virtualenvwrapper # Extensions to Ian Bicking's virtualenv tool
+AddPackage pypy3 # A Python3 implementation written in Python, JIT enabled
