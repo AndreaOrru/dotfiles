5f0ae56546c078feeb0b596660a859f8f14cdd97
Andrea Orru <andrea@orru.io>
2020-05-26 18:32:24 +0900

    Counsel Dash Rust
    Former-commit-id: 0463d4b6f810ed4b48552d127ee686f7f54c023e

--- emacs/.emacs.d/config/init-rust.el
+++ emacs/.emacs.d/config/init-rust.el
@@ -4,4 +4,8 @@
   (setq lsp-rust-analyzer-cargo-watch-command "clippy")
   (setq rustic-format-on-save t))
 
+(after 'init-docs
+  (add-hook 'rustic-mode-hook
+            #'(lambda () (setq-local counsel-dash-docsets '("Rust")))))
+
 (provide 'init-rust)
