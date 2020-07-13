30afdc08bec260eb3fc7179b59c8927eb9770391
Andrea Orru <andrea@orru.io>
2020-07-13 22:41:06 +0900

    Remove this auto-balancing non-sense
    Former-commit-id: af39c588062107b8912c12d8329b73e49fdb8e4b

--- emacs/.emacs.d/config/init-windows.el
+++ emacs/.emacs.d/config/init-windows.el
@@ -11,12 +11,6 @@
   (which-key/describe-prefix "w" "windows")
   (evil-leader/set-key "ws" 'popwin:stick-popup-window))
 
-;; Keep windows balanced.
-(require-package 'balanced-windows)
-(add-hook 'after-init-hook 'balanced-windows-mode)
-(after 'balanced-windows
-  (setq balanced-windows-functions '(delete-window quit-window)))
-
 ;; Split and switch to the new window immediately.
 (global-set-key (kbd "C-x 2")
                 #'(lambda () (interactive) (evil-window-split) (other-window 1)))
