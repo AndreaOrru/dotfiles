00ab38b778cc5c387be951916dd0791e1d242375
Andrea Orru <andrea@orru.io>
2020-07-16 19:59:03 +0900

    Noise cancelling
    Former-commit-id: ed6716bc08f69e269cad93d57bbfcbce1cab8ca3

--- aconfmgr/.config/aconfmgr/20-hardware.sh
+++ aconfmgr/.config/aconfmgr/20-hardware.sh
@@ -68,8 +68,9 @@ CopyFile /etc/profile.d/libva-driver.sh
 AddPackage sof-firmware # Sound Open Firmware
 CopyFile /etc/modprobe.d/no-wrong-audio.conf
 
-# Automatically switch to newly connected audio devices, enable SOF.
+# Switch to newly connected audio devices, enable SOF, noise cancelling, no resampling.
 CopyFile /etc/pulse/default.pa
+CopyFile /etc/pulse/daemon.conf
 
 
 ###################
