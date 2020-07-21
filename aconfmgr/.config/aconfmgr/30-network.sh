d7c545e14041022468096884cc63d4c285a8f15b
Andrea Orru <andrea@orru.io>
2020-07-21 22:28:34 +0900

    bind-tools -> bind
    Former-commit-id: 2040ad9ee2ff9717647d2cca3b7407ea9d9c766f

--- aconfmgr/.config/aconfmgr/30-network.sh
+++ aconfmgr/.config/aconfmgr/30-network.sh
@@ -4,7 +4,7 @@
 
 AddPackage aria2 # Download utility that supports HTTP(S), FTP, BitTorrent, and Metalink
 AddPackage autossh # Automatically restart SSH sessions and tunnels
-AddPackage bind-tools # The ISC DNS tools
+AddPackage bind # The ISC DNS Server
 AddPackage elinks # An advanced and well-established feature-rich text mode web browser.
 AddPackage nmap # Utility for network discovery and security auditing
 AddPackage openvpn # An easy-to-use, robust and highly configurable VPN (Virtual Private Network)
