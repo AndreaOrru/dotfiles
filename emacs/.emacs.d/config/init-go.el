c05cfe7583a711e561d7062d5b14125716132ae8
Andrea Orru <andrea@orru.io>
2020-10-12 13:45:35 +0900

    Change order of hooks
    Former-commit-id: e19ecacdf1ebb9afb3211b9c83e051d4ce2f6ce2

--- emacs/.emacs.d/config/init-go.el
+++ emacs/.emacs.d/config/init-go.el
@@ -9,7 +9,8 @@
               (evil-local-set-key 'normal (kbd "gD") 'godef-jump-other-window)
               ;; Format code on saving.
               (add-hook 'before-save-hook 'lsp-format-buffer t t)
-              (add-hook 'before-save-hook 'lsp-organize-imports t t)))
+              (add-hook 'before-save-hook 'lsp-organize-imports t t))
+          98)
 
 ;; Recenter screen after jumping to definition.
 (advice-add
