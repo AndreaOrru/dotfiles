1d2345a5f75736962594a3effb645b7227f34430
Andrea Orru <andrea@orru.io>
2020-10-12 13:25:25 +0900

    init-go.el
    Former-commit-id: d8a330beb6336ff58c66d534cfe5aea9e66db3e2

---
+++ emacs/.emacs.d/config/init-go.el
@@ -0,0 +1,19 @@
+;; -*- lexical-binding: t -*-
+
+(require-package 'go-mode)
+(add-hook 'go-mode-hook 'lsp)
+
+(add-hook 'go-mode-hook
+          #'(lambda ()
+              ;; Jump to definition in another window.
+              (evil-local-set-key 'normal (kbd "gD") 'godef-jump-other-window)
+              ;; Format code on saving.
+              (add-hook 'before-save-hook 'lsp-format-buffer t t)
+              (add-hook 'before-save-hook 'lsp-organize-imports t t)))
+
+;; Recenter screen after jumping to definition.
+(advice-add
+ 'godef-jump :after
+ #'(lambda (orig-fun &rest args) (recenter)))
+
+(provide 'init-go)
