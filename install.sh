9f326ad55686cddf472e9d13bf9cbb89fb7998e6
Andrea Orru <andrea@orru.io>
2020-10-16 21:28:31 +0900

    rofi
    Former-commit-id: 2fcd2469585a9241a557b9ed9e0195ae7c43abfd

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ chmod 600 secrets/.ssh/*
 chmod 644 secrets/.ssh/id_rsa.pub
 stow secrets
 
-stow binaries
 stow emacs
 stow git
 stow kitty
 stow mpv
 stow pastebinit
+stow rofi
 stow sway
 stow tmux
 stow zsh
