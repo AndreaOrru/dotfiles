4d2506bb029e792a211a799fe14f5bb2ccb08835
Andrea Orru <andrea@orru.io>
2020-10-21 15:39:03 +0900

    rcoverage.sh
    Former-commit-id: 9f640ab3c0c9779b61238bcd6f358d18b05b3780

---
+++ binaries/bin/rcoverage.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export RUSTDOCFLAGS="-Cpanic=abort"
+
+cargo clean
+cargo build
+cargo test
+
+grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing --ignore "tests/**" -o ./target/debug/coverage/
