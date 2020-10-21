53297d873ba1eed57f5694e626380a6a04cab2d1
Andrea Orru <andrea@orru.io>
2020-10-21 19:48:57 +0900

    devel-update.sh
    Former-commit-id: 7c986d5a4d1313a7c5c9eb496c0c4a17429b165b

--- binaries/bin/devel-update.sh
+++ binaries/bin/devel-update.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-pacman -Qmq | grep -Ee '-(cvs|svn|git|hg|bzr|darcs)$' | grep -v ttf-google-fonts-git | grep -v emacs-pgtk-native-comp-git | yay -S --needed -
+pacman -Qmq \
+    | grep -Ee '-(cvs|svn|git|hg|bzr|darcs)$' \
+    | grep -v ttf-google-fonts-git \
+    | grep -v emacs-pgtk-native-comp-git \
+    | yay -S --needed -
